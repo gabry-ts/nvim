64f07acc487dfd7a509c73c5e34bd2e7fd2184bf
Gabriele <gabrielepartiti+github@icloud.com>
2024-09-16 23:40:17 +0200

    feat: add key bindings for copilot chat and commands

--- nvim/lua/plugins/copilot-chat.lua
+++ nvim/lua/plugins/copilot-chat.lua
@@ -1,3 +1,21 @@
+local wk = require("which-key")
+
+wk.add({
+  { "<leader>m", group = "CopilotChat" },
+
+  { "<leader>mc", "<cmd>CopilotChat<cr>", desc = "Chat" },
+  { "<leader>mL", "<cmd>CopilotChatLoad<cr>", desc = "Load chat history from file" },
+  { "<leader>mS", "<cmd>CopilotChatSave<cr>", desc = "Save chat history to file" },
+  -- { "<leader>md", "<cmd>CopilotChatDocs<cr>", desc = "Add documentation to selection", mode = "v" },
+  { "<leader>me", "<cmd>CopilotChatExplain<cr>", desc = "Explain selected code", mode = "v" },
+  { "<leader>mf", "<cmd>CopilotChatFix<cr>", desc = "Fix selected code", mode = "v" },
+  { "<leader>mm", "<cmd>CopilotChatModels<cr>", desc = "View and select available models" },
+  { "<leader>mo", "<cmd>CopilotChatOptimize<cr>", desc = "Optimize selected code", mode = "v" },
+  -- { "<leader>mr", "<cmd>CopilotChatReview<cr>", desc = "Review selected code", mode = "v" },
+  -- { "<leader>ms", "<cmd>CopilotChatStop<cr>", desc = "Stop current copilot output" },
+  { "<leader>mt", "<cmd>CopilotChatTests<cr>", desc = "Generate tests for code" },
+}, { prefix = "<leader>" })
+
 return {
   {
     "CopilotC-Nvim/CopilotChat.nvim",
@@ -10,6 +28,5 @@ return {
       debug = true, -- Enable debugging
       -- See Configuration section for rest
     },
-    -- See Commands section for default commands if you want to lazy load on them
   },
 }
