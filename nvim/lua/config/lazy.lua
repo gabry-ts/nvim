d4596fd4a5fb8317134f64e486465dd1e5102aa7
Gabriele <gabrielepartiti+github@icloud.com>
2024-09-16 23:21:44 +0200

    refactor: remove redundant import comments

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -25,8 +25,6 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.ui.edgy" },
     -- { import = "lazyvim.plugins.extras.ui.mini-indentscope" },
 
-    -- { import = "lazyvim.plugins.extras.ui.edgy" },
-    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
   },
