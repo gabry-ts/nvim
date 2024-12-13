42d696db3703bc689f3e8164a118a2a611f0c2bf
Gabriele <gabrielepartiti+github@icloud.com>
2024-12-13 01:10:11 +0100

    refactor: replace Telescope with FzfLua for find commands

--- nvim/lua/plugins/starter.lua
+++ nvim/lua/plugins/starter.lua
@@ -40,42 +40,6 @@ return {
     end,
   },
 
-  -- change some telescope options and a keymap to browse plugin files
-  {
-    "nvim-telescope/telescope.nvim",
-    keys = {
-      -- add a keymap to browse plugin files
-      -- stylua: ignore
-      {
-        "<leader>fp",
-        function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
-        desc = "Find Plugin File",
-      },
-    },
-    -- change some options
-    opts = {
-      pickers = {
-        colorscheme = {
-          enable_preview = true,
-        },
-      },
-      defaults = {
-        file_ignore_patterns = {
-          "^node_modules/",
-        },
-        layout_strategy = "horizontal",
-        layout_config = {
-          prompt_position = "top",
-          horizontal = {
-            preview_cutoff = 0,
-          },
-        },
-        sorting_strategy = "ascending",
-        winblend = 0,
-      },
-    },
-  },
-
   -- add pyright to lspconfig
   {
     "neovim/nvim-lspconfig",
