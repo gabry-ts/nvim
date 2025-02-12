a54130dc0586ba76a100afa8ca736b8fab2507a8
Gabriele <gabrielepartiti+github@icloud.com>
2025-02-12 14:38:02 +0100

    chore: update version to 8 in lazyvim.json

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -65,7 +65,8 @@ return {
       use_libuv_file_watcher = true,
     },
     window = {
-      position = "right",
+      position = "left",
+
       mappings = {
         ["l"] = "open",
         ["h"] = "close_node",
