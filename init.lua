48f5de29a279d40ee9463585f906c18b8aa3c126
Gabriele <gabrielepartiti+github@icloud.com>
2025-04-16 18:25:15 +0200

    feat: add initial configuration for augment code workspace

--- init.lua
+++ init.lua
@@ -1,2 +1,5 @@
+-- AI
+vim.g.augment_workspace_folders = { "~/Developer/WebstormProjects/enhancers/visura/lextel-ai/packages/backend/server/" }
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
