4ffd40eb398749b56adc52982d85c055298bc4d5
Gabriele <gabrielepartiti+github@icloud.com>
2025-09-08 01:06:10 +0200

    feat: enable Ruff for Python diagnostics in LazyVim

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -11,3 +11,5 @@ vim.g.lazyvim_rust_diagnostics = "rust-analyzer"
 
 -- set wrap as default
 vim.opt.wrap = true
+
+vim.g.lazyvim_python_ruff = "ruff"
--- lua/plugins/starter.lua
+++ lua/plugins/starter.lua
@@ -43,12 +43,12 @@ return {
   -- add pyright to lspconfig
   {
     "neovim/nvim-lspconfig",
+
     ---@class PluginLspOpts
     opts = {
       ---@type lspconfig.options
       servers = {
         -- pyright will be automatically installed with mason and loaded with lspconfig
-        pyright = {},
       },
       bacon_ls = {
         enabled = diagnostics == "bacon-ls",
